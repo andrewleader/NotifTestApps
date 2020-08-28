7706c753b31bf13916866f9b999425502026cfa1
Andrew Leader <aleader@microsoft.com>
2020-08-28 16:56:51 -0700

    Added .NET Core DesktopCompat

--- CS/WPF.NetCore.DesktopCompat/MainWindow.xaml.cs
+++ CS/WPF.NetCore.DesktopCompat/MainWindow.xaml.cs
@@ -1,28 +1,24 @@
-﻿// ******************************************************************
-// Copyright (c) Microsoft. All rights reserved.
-// This code is licensed under the MIT License (MIT).
-// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
-// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
-// ******************************************************************
-
-using Microsoft.QueryStringDotNET;
+﻿using Microsoft.QueryStringDotNET;
 using Microsoft.Toolkit.Uwp.Notifications;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using Windows.Data.Xml.Dom;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 using Windows.UI.Notifications;
 
-namespace DesktopToastsApp
+namespace WPF.NetCore.DesktopCompat
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
