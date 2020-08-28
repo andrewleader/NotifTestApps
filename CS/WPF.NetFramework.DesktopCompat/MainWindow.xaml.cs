ddf09103312acdb5d485c2a1a93ef7d22e9391ea
Andrew Leader <aleader@microsoft.com>
2020-08-28 16:31:01 -0700

    Added existing app

---
+++ CS/WPF.NetFramework.DesktopCompat/MainWindow.xaml.cs
@@ -0,0 +1,212 @@
+﻿// ******************************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+
+using Microsoft.QueryStringDotNET;
+using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace DesktopToastsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // IMPORTANT: Look at App.xaml.cs for required registration and activation steps
+        }
+
+        private async void ButtonPopToast_Click(object sender, RoutedEventArgs e)
+        {
+            string title = "Andrew sent you a picture";
+            string content = "Check this out, The Enchantments!";
+            string image = "https://picsum.photos/364/202?image=883";
+            int conversationId = 5;
+
+            // Construct the toast content
+            ToastContent toastContent = new ToastContent()
+            {
+                // Arguments when the user taps body of toast
+                Launch = new QueryString()
+                {
+                    { "action", "viewConversation" },
+                    { "conversationId", conversationId.ToString() }
+
+                }.ToString(),
+
+                Visual = new ToastVisual()
+                {
+                    BindingGeneric = new ToastBindingGeneric()
+                    {
+                        Children =
+                        {
+                            new AdaptiveText()
+                            {
+                                Text = title
+                            },
+
+                            new AdaptiveText()
+                            {
+                                Text = content
+                            },
+
+                            new AdaptiveImage()
+                            {
+                                // Non-Desktop Bridge apps cannot use HTTP images, so
+                                // we download and reference the image locally
+                                Source = await DownloadImageToDisk(image)
+                            }
+                        },
+
+                        AppLogoOverride = new ToastGenericAppLogo()
+                        {
+                            Source = await DownloadImageToDisk("https://unsplash.it/64?image=1005"),
+                            HintCrop = ToastGenericAppLogoCrop.Circle
+                        }
+                    }
+                },
+
+                Actions = new ToastActionsCustom()
+                {
+                    Inputs =
+                    {
+                        new ToastTextBox("tbReply")
+                        {
+                            PlaceholderContent = "Type a response"
+                        }
+                    },
+
+                    Buttons =
+                    {
+                        // Note that there's no reason to specify background activation, since our COM
+                        // activator decides whether to process in background or launch foreground window
+                        new ToastButton("Reply", new QueryString()
+                        {
+                            { "action", "reply" },
+                            { "conversationId", conversationId.ToString() }
+
+                        }.ToString()),
+
+                        new ToastButton("Like", new QueryString()
+                        {
+                            { "action", "like" },
+                            { "conversationId", conversationId.ToString() }
+
+                        }.ToString()),
+
+                        new ToastButton("View", new QueryString()
+                        {
+                            { "action", "viewImage" },
+                            { "imageUrl", image }
+
+                        }.ToString())
+                    }
+                }
+            };
+
+            // Make sure to use Windows.Data.Xml.Dom
+            var doc = new XmlDocument();
+            doc.LoadXml(toastContent.GetContent());
+
+            // And create the toast notification
+            var toast = new ToastNotification(doc);
+
+            // And then show it
+            DesktopNotificationManagerCompat.CreateToastNotifier().Show(toast);
+        }
+
+        private static bool _hasPerformedCleanup;
+        private static async Task<string> DownloadImageToDisk(string httpImage)
+        {
+            // Toasts can live for up to 3 days, so we cache images for up to 3 days.
+            // Note that this is a very simple cache that doesn't account for space usage, so
+            // this could easily consume a lot of space within the span of 3 days.
+
+            try
+            {
+                if (DesktopNotificationManagerCompat.CanUseHttpImages)
+                {
+                    return httpImage;
+                }
+
+                var directory = Directory.CreateDirectory(System.IO.Path.GetTempPath() + "WindowsNotifications.DesktopToasts.Images");
+
+                if (!_hasPerformedCleanup)
+                {
+                    // First time we run, we'll perform cleanup of old images
+                    _hasPerformedCleanup = true;
+
+                    foreach (var d in directory.EnumerateDirectories())
+                    {
+                        if (d.CreationTimeUtc.Date < DateTime.UtcNow.Date.AddDays(-3))
+                        {
+                            d.Delete(true);
+                        }
+                    }
+                }
+
+                var dayDirectory = directory.CreateSubdirectory(DateTime.UtcNow.Day.ToString());
+                string imagePath = dayDirectory.FullName + "\\" + (uint)httpImage.GetHashCode();
+
+                if (File.Exists(imagePath))
+                {
+                    return imagePath;
+                }
+
+                HttpClient c = new HttpClient();
+                using (var stream = await c.GetStreamAsync(httpImage))
+                {
+                    using (var fileStream = File.OpenWrite(imagePath))
+                    {
+                        stream.CopyTo(fileStream);
+                    }
+                }
+
+                return imagePath;
+            }
+            catch { return ""; }
+        }
+
+        internal void ShowConversation()
+        {
+            ContentBody.Content = new TextBlock()
+            {
+                Text = "You've just opened a conversation!",
+                FontWeight = FontWeights.Bold
+            };
+        }
+
+        internal void ShowImage(string imageUrl)
+        {
+            ContentBody.Content = new Image()
+            {
+                Source = new BitmapImage(new Uri(imageUrl))
+            };
+        }
+
+        private void ButtonClearToasts_Click(object sender, RoutedEventArgs e)
+        {
+            DesktopNotificationManagerCompat.History.Clear();
+        }
+    }
+}
