7fa3fb207663966d10704a2fa35d26ee332b4a4d
Andrew Leader <aleader@microsoft.com>
2020-08-28 18:01:27 -0700

    NetCore ToastCompat

---
+++ CS/WPF.NetCore.ToastCompat/App.xaml.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.QueryStringDotNET;
+using Microsoft.Toolkit.Uwp.Notifications;
+using System.Windows;
+using Windows.UI.Notifications;
+
+namespace WPF.NetCore.ToastCompat
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ToastNotificationManagerCompat.OnActivated += ToastNotificationManagerCompat_OnActivated;
+
+            // If launched from a toast
+            if (ToastNotificationManagerCompat.WasCurrentProcessToastActivated())
+            {
+                // Our OnActivated callback will run after this completes,
+                // and will show a window if necessary.
+            }
+
+            else
+            {
+                // Show the window
+                // In App.xaml, be sure to remove the StartupUri so that a window doesn't
+                // get created by default, since we're creating windows ourselves (and sometimes we
+                // don't want to create a window if handling a background activation).
+                new MainWindow().Show();
+            }
+        }
+
+        private void ToastNotificationManagerCompat_OnActivated(ToastNotificationActivatedEventArgsCompat e)
+        {
+            Dispatcher.Invoke(delegate
+            {
+                if (e.Argument.Length == 0)
+                {
+                    OpenWindowIfNeeded();
+                    return;
+                }
+
+                // Parse the query string (using NuGet package QueryString.NET)
+                QueryString args = QueryString.Parse(e.Argument);
+
+                // See what action is being requested 
+                switch (args["action"])
+                {
+                    // Open the image
+                    case "viewImage":
+
+                        // The URL retrieved from the toast args
+                        string imageUrl = args["imageUrl"];
+
+                        // Make sure we have a window open and in foreground
+                        OpenWindowIfNeeded();
+
+                        // And then show the image
+                        (App.Current.Windows[0] as MainWindow).ShowImage(imageUrl);
+
+                        break;
+
+                    // Open the conversation
+                    case "viewConversation":
+
+                        // The conversation ID retrieved from the toast args
+                        int conversationId = int.Parse(args["conversationId"]);
+
+                        // Make sure we have a window open and in foreground
+                        OpenWindowIfNeeded();
+
+                        // And then show the conversation
+                        (App.Current.Windows[0] as MainWindow).ShowConversation();
+
+                        break;
+
+                    // Background: Quick reply to the conversation
+                    case "reply":
+
+                        // Get the response the user typed
+                        string msg = e.UserInput["tbReply"] as string;
+
+                        // And send this message
+                        ShowToast("Message sent: " + msg);
+
+                        // If there's no windows open, exit the app
+                        if (App.Current.Windows.Count == 0)
+                        {
+                            Application.Current.Shutdown();
+                        }
+
+                        break;
+
+                    // Background: Send a like
+                    case "like":
+
+                        ShowToast("Like sent!");
+
+                        // If there's no windows open, exit the app
+                        if (App.Current.Windows.Count == 0)
+                        {
+                            Application.Current.Shutdown();
+                        }
+
+                        break;
+
+                    default:
+
+                        OpenWindowIfNeeded();
+
+                        break;
+                }
+            });
+        }
+
+        private void OpenWindowIfNeeded()
+        {
+            // Make sure we have a window open (in case user clicked toast while app closed)
+            if (App.Current.Windows.Count == 0)
+            {
+                new MainWindow().Show();
+            }
+
+            // Activate the window, bringing it to focus
+            App.Current.Windows[0].Activate();
+
+            // And make sure to maximize the window too, in case it was currently minimized
+            App.Current.Windows[0].WindowState = WindowState.Normal;
+        }
+
+        private void ShowToast(string msg)
+        {
+            // Construct the visuals of the toast
+            ToastContent toastContent = new ToastContentBuilder()
+                .AddToastActivationInfo("action=ok", ToastActivationType.Foreground)
+                .AddText(msg)
+                .GetToastContent();
+
+            // And create the toast notification
+            var toast = new ToastNotification(toastContent.GetXml());
+
+            // And then show it
+            ToastNotificationManagerCompat.CreateToastNotifier().Show(toast);
+        }
+    }
+}
---
+++ CS/WPF.NetCore.ToastCompat/MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.QueryStringDotNET;
+using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.UI.Notifications;
+
+namespace WPF.NetCore.ToastCompat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // IMPORTANT: Look at App.xaml.cs for activation
+        }
+
+        private async void ButtonPopToast_Click(object sender, RoutedEventArgs e)
+        {
+            string title = "Andrew sent you a picture";
+            string content = "Check this out, The Enchantments!";
+            string image = "https://picsum.photos/364/202?image=883";
+            int conversationId = 5;
+
+            // Construct the toast content
+            ToastContent toastContent = new ToastContentBuilder()
+                .AddToastActivationInfo(new QueryString()
+                {
+                    { "action", "viewConversation" },
+                    { "conversationId", conversationId.ToString() }
+
+                }.ToString(), ToastActivationType.Foreground) // Arguments when the user taps body of toast
+                .AddText(title)
+                .AddText(content)
+                .AddInlineImage(new Uri(await DownloadImageToDisk(image)))
+                .AddAppLogoOverride(new Uri(await DownloadImageToDisk("https://unsplash.it/64?image=1005")), ToastGenericAppLogoCrop.Circle)
+                .AddInputTextBox("tbReply", "Type a reply")
+
+                // Note that there's no reason to specify background activation, since our COM
+                // activator decides whether to process in background or launch foreground window
+                .AddButton(new ToastButton("Reply", new QueryString()
+                {
+                    { "action", "reply" },
+                    { "conversationId", conversationId.ToString() }
+
+                }.ToString()))
+                .AddButton(new ToastButton("Like", new QueryString()
+                {
+                    { "action", "like" },
+                    { "conversationId", conversationId.ToString() }
+
+                }.ToString()))
+                .AddButton(new ToastButton("View", new QueryString()
+                {
+                    { "action", "viewImage" },
+                    { "imageUrl", image }
+
+                }.ToString()))
+                .GetToastContent();
+
+            // And create the toast notification
+            var toast = new ToastNotification(toastContent.GetXml());
+
+            // And then show it
+            ToastNotificationManagerCompat.CreateToastNotifier().Show(toast);
+        }
+
+        private static bool _hasPerformedCleanup;
+        private static async Task<string> DownloadImageToDisk(string httpImage)
+        {
+            // Toasts can live for up to 3 days, so we cache images for up to 3 days.
+            // Note that this is a very simple cache that doesn't account for space usage, so
+            // this could easily consume a lot of space within the span of 3 days.
+
+            try
+            {
+                if (ToastNotificationManagerCompat.CanUseHttpImages)
+                {
+                    return httpImage;
+                }
+
+                var directory = Directory.CreateDirectory(System.IO.Path.GetTempPath() + "WindowsNotifications.DesktopToasts.Images");
+
+                if (!_hasPerformedCleanup)
+                {
+                    // First time we run, we'll perform cleanup of old images
+                    _hasPerformedCleanup = true;
+
+                    foreach (var d in directory.EnumerateDirectories())
+                    {
+                        if (d.CreationTimeUtc.Date < DateTime.UtcNow.Date.AddDays(-3))
+                        {
+                            d.Delete(true);
+                        }
+                    }
+                }
+
+                var dayDirectory = directory.CreateSubdirectory(DateTime.UtcNow.Day.ToString());
+                string imagePath = dayDirectory.FullName + "\\" + (uint)httpImage.GetHashCode();
+
+                if (File.Exists(imagePath))
+                {
+                    return imagePath;
+                }
+
+                HttpClient c = new HttpClient();
+                using (var stream = await c.GetStreamAsync(httpImage))
+                {
+                    using (var fileStream = File.OpenWrite(imagePath))
+                    {
+                        stream.CopyTo(fileStream);
+                    }
+                }
+
+                return imagePath;
+            }
+            catch { return ""; }
+        }
+
+        internal void ShowConversation()
+        {
+            ContentBody.Content = new TextBlock()
+            {
+                Text = "You've just opened a conversation!",
+                FontWeight = FontWeights.Bold
+            };
+        }
+
+        internal void ShowImage(string imageUrl)
+        {
+            ContentBody.Content = new Image()
+            {
+                Source = new BitmapImage(new Uri(imageUrl))
+            };
+        }
+
+        private void ButtonClearToasts_Click(object sender, RoutedEventArgs e)
+        {
+            ToastNotificationManagerCompat.History.Clear();
+        }
+    }
+}
